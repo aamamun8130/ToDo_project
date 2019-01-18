ab37f580732b998936d22afd2480636da99ec06f
aamamun8130 <md.abdullah8130@gmail.com>
2019-01-18 19:02:47 +0600

    remove activity element

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ function $(id) {
     return document.getElementById(id);
 }
 
+var id=0;
+
 function addActivity() {
-    $('actContainer').innerHTML += '<li>' +
-    '<span id="activity">Item 1</span>' +
+    id =id + 1;
+    var inputActivity= $("iputActivity").value;
+    $('actContainer').innerHTML += '<li id="'+id+'">' +
+    '<span id="#" >' + inputActivity + '</span>' +
     '<i class="fa fa-check-circle" style="padding-left:15px; color:aqua; float: right; height: 50px;"></i>' +
-    '<i class="fa fa-trash" aria-hidden="true" style="border-right: 2px solid violet; padding: 5px 15px; border-color: lightgrey; float: right"></i>' +
+    '<i id="'+id+'" onclick="removeElement('+this.id+');" class="fa fa-trash" aria-hidden="true" style="border-right: 2px solid violet; padding: 5px 15px; border-color: lightgrey; float: right"></i>' +
 '</li>';
-    $('activity').innerHTML = $("iputActivity").value;
+   
+}
+
+function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+    //console.log(elementId);
 }

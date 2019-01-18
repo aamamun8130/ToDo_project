bad6170e4ceab6b4a4d9166afd0685a427bf86fb
aamamun8130 <md.abdullah8130@gmail.com>
2019-01-18 18:19:20 +0600

    Add new activity element js

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ function $(id) {
 }
 
 function addActivity() {
+    $('actContainer').innerHTML += '<li>' +
+    '<span id="activity">Item 1</span>' +
+    '<i class="fa fa-check-circle" style="padding-left:15px; color:aqua; float: right; height: 50px;"></i>' +
+    '<i class="fa fa-trash" aria-hidden="true" style="border-right: 2px solid violet; padding: 5px 15px; border-color: lightgrey; float: right"></i>' +
+'</li>';
     $('activity').innerHTML = $("iputActivity").value;
 }
